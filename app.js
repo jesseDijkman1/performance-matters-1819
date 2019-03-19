4b2d76c4d0881d499f84f99ae415a16556cac8eb
Jesse Dijkman <jesse.dijkman@hva.nl>
2019-03-19 12:02:34 +0100

    I now use a temporary json file for making requests, removed a package from project

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const request = require("request");
 const ejs = require("ejs");
 const parseString = require('xml2js').parseString;
 const fs = require("fs");
-// const API = require("oba-wrapper/node");
 
 require('dotenv').config();
 
@@ -29,10 +28,6 @@ app.get("/", (req, res) => {
   });
 })
 
-app.get("/results", (req, res) => {
-  res.send("Results page")
-})
-
 app.post("/submitWord", (req, res) => {
   const singleWord = req.body.dataWord;
   let allWords = req.body.wordsBundle;
@@ -46,7 +41,6 @@ app.post("/submitWord", (req, res) => {
     allWords += `,${singleWord}`;
     allWords = allWords.split(",");
   }
-
 
   res.render("index.ejs", {
     words: allWords
@@ -66,37 +60,18 @@ app.post("/removeWord", (req, res) => {
 })
 
 app.post("/getResults", (req, res) => {
-  const queryBase = `https://zoeken.oba.nl/api/v1/search?authorization=${process.env.API_KEY}`;
-  const queryDefaults = "&pagesize=20&refine=true";
-  let page = "&page=1";
-  let query;
+  fs.readFile("public/data/temp.json", (err, data) => {
+    let parsedData = JSON.parse(data);
 
-  let words = req.body.wordsBundle || "a";
-  // console.log("words", words, words.length)
+    parsedData.aquabrowser.meta[0].totalPages = Math.ceil(parsedData.aquabrowser.meta[0].count / 20);
 
-  if (words.length > 1) {
-    words = words.split(",");
-    words = `&q=${words.join("+")}`;
-  } else {
-    words = `&q=${words}`
-  }
-
-  query = queryBase + words + queryDefaults + page;
-
-  request(query, (error, response, body) => {
-    parseString(body, (err, result) => {
-      result.aquabrowser.meta[0].totalPages = Math.ceil(result.aquabrowser.meta[0].count / 20);
-
-      let data = {
-        meta: result.aquabrowser.meta[0],
-        results: result.aquabrowser.results[0].result
+    res.render("list.ejs", {
+      data: {
+        meta: parsedData.aquabrowser.meta[0],
+        results: parsedData.aquabrowser.results[0].result
       }
-
-      res.render("list.ejs", {
-        data: data
-      });
     });
-  });
+  })
 })
 
 app.listen(port, () => console.log(`Listening to port: ${port}!`))
